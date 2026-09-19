291f1663f2fb611fd47011ce3ee251606dddf800
oabaturov-ops <oabaturov@gmail.com>
2026-09-19 22:37:43 +0500

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,7 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: ""https://books.abaturministry.org"",
+      url: "https://books.abaturministry.org",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
